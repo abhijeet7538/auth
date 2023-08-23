d93f987237d63f0c824b6adc689748c8dc27982b
Abhijeet <carnage5773@gmail.com>
2023-08-23 12:42:44 +0530

    add environment variables-dotemv

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const express = require('express');
 const ejs = require('ejs');
@@ -19,8 +20,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const cigret = 'bukiburmeebillykaball';
-userSchema.plugin(encrypt, { secret: cigret, encryptedFields: ['password'] });
+userSchema.plugin(encrypt, { secret: process.env.CIGRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model('Usser', userSchema);
 
